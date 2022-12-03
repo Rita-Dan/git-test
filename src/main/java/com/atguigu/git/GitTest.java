d99f4035fd12ff36eb34ce80439141ec23bc5fe8
Rita <zrdcoder@163.com>
2022-12-03 14:54:17 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");//文件修改后文件名变蓝色，代表“被追踪过”，再add再commit
+        System.out.println("hello git3!");//文件修改后文件名变蓝色，代表“被追踪过”，直接提交本地库 git-test上commit
     }
 }
