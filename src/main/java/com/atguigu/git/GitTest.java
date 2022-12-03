4405acd54bfaaf25074dadd791ec8e7f97b2e718
Rita <58017092+Rita-Dan@users.noreply.github.com>
2022-12-03 18:13:02 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git2!");//文件修改后文件名变蓝色，代表“被追踪过”，再add再commit
         System.out.println("push test!");//文件修改后文件名变蓝色，代表“被追踪过”
+        System.out.println("pull test!");//文件在GitHub上修改的
     }
 }
