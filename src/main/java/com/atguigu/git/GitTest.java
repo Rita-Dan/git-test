0727bb799829462ddd2e4103b7f7b64d59860cee
Rita <zrdcoder@163.com>
2022-12-03 14:21:39 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");//文件修改后文件名变蓝色，代表“被追踪过”，再add再commit
     }
 }
