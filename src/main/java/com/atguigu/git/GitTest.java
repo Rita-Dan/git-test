95c4a28f3fb7ee03ede7ed6489796fbf32a72fbb
Rita <zrdcoder@163.com>
2022-12-03 16:35:58 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");//文件修改后文件名变蓝色，代表“被追踪过”，再add再commit
+        System.out.println("push test!");//文件修改后文件名变蓝色，代表“被追踪过”
     }
 }
